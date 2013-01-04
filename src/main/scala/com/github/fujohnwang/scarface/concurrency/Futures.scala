3b9761054ef1d9e3ea5e3dc0866924d19dcb78ae
DarrenWang <fujohnwang@gmail.com>
2013-01-04 20:44:32 +0800

    future sample change
    future sample change

--- src/main/scala/com/github/fujohnwang/scarface/concurrency/Futures.scala
+++ src/main/scala/com/github/fujohnwang/scarface/concurrency/Futures.scala
@@ -1,22 +1,38 @@
 package com.github.fujohnwang.scarface.concurrency
 
 import concurrent._
-import java.util.concurrent.TimeUnit
+import duration._
+import java.util.concurrent.{TimeoutException, TimeUnit}
 
 object Futures {
   def main(args: Array[String]) {
     import ExecutionContext.Implicits.global
 
+    //    val f = future {
+    //      TimeUnit.SECONDS.sleep(1)
+    //      1
+    //    }
+    //    f.onSuccess {
+    //      case i: Int => println("result receives")
+    //    }
+    //
+    //    println("wait for complete, but may be executed before the callback.")
+    //
+    //    TimeUnit.SECONDS.sleep(2)
+
+
     val f = future {
-      TimeUnit.SECONDS.sleep(1)
-      1
-    }
-    f.onSuccess {
-      case i: Int => println("result receives")
+      println("raw computing")
+      throw new Exception()
+
+    } recoverWith {
+      case _ => future {
+        println("recover with ")
+        2
+      }
     }
 
-    println("wait for complete, but may be executed before the callback.")
 
-    TimeUnit.SECONDS.sleep(2)
+    Await.result(f, 5 minute)
   }
 }
