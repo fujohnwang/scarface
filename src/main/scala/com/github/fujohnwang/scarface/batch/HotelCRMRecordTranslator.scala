5a5698a7395e4faed345be5fa46cc80f6ebde6c1
fujohnwang <fujohnwang@gmail.com>
2013-11-11 13:50:04 +0800

    add data transformer

--- src/main/scala/com/github/fujohnwang/scarface/batch/HotelCRMRecordTranslator.scala
+++ src/main/scala/com/github/fujohnwang/scarface/batch/HotelCRMRecordTranslator.scala
@@ -3,11 +3,13 @@ package com.github.fujohnwang.scarface.batch
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource
 import org.springframework.jdbc.core.{RowCallbackHandler, JdbcTemplate}
 import java.sql.ResultSet
+import org.elasticsearch.node.NodeBuilder._
+import java.util.{HashMap => JMap}
 
 object HotelCRMRecordTranslator {
   def main(args: Array[String]) {
-//    println(System.getProperty("java.library.path"))
-//    System.loadLibrary("sqljdbc_auth.dll")
+    //    println(System.getProperty("java.library.path"))
+    //    System.loadLibrary("sqljdbc_auth.dll")
 
     val dataSource = new SQLServerDataSource
     dataSource.setServerName("10.72.87.96")
@@ -15,48 +17,58 @@ object HotelCRMRecordTranslator {
     dataSource.setPassword("111111")
     dataSource.setDatabaseName("shifenzheng")
     // this need to load dll, so disable it since I don't care security issue under LAN
-//    dataSource.setIntegratedSecurity(true)
+    //    dataSource.setIntegratedSecurity(true)
 
 
     val jt = new JdbcTemplate(dataSource)
+    val node = nodeBuilder().clusterName("afoosearch").client(true).node()
+    val client = node.client()
+    try {
+      jt.query("select * from cdsgus", new RowCallbackHandler {
+        def processRow(rs: ResultSet) {
+          val map = new JMap[String, Object]
 
-    jt.query("select top 10 * from cdsgus", new RowCallbackHandler {
-      def processRow(rs: ResultSet) {
-        val name = rs.getString("Name")
-        val cardNo = rs.getString("CardNo")
-        val descriot = rs.getString("Descriot")
-        val ctftp = rs.getString("CtfTp")
-        val ctfid = rs.getString("CtfId")
-        val gender = rs.getString("Gender")
-        val birthday = rs.getString("Birthday")
-        val address = rs.getString("Address")
-        val zip = rs.getString("Zip")
-        val dirty = rs.getString("Dirty")
-        val district1 = rs.getString("District1")
-        val district2 = rs.getString("District2")
-        val district3 = rs.getString("District3")
-        val district4 = rs.getString("District4")
-        val district5 = rs.getString("District5")
-        val district6 = rs.getString("District6")
-        val firstName = rs.getString("FirstNm")
-        val lastName = rs.getString("LastNm")
-        val duty = rs.getString("Duty")
-        val mobile = rs.getString("Mobile")
-        val tel = rs.getString("Tel")
-        val fex = rs.getString("Fax")
-        val email = rs.getString("EMail")
-        val nation = rs.getString("Nation")
-        val taste = rs.getString("Taste")
-        val edu = rs.getString("Education")
-        val com = rs.getString("Company")
-        val ctel = rs.getString("CTel")
-        val caddress = rs.getString("CAddress")
-        val czip = rs.getString("CZip")
-        val family = rs.getString("Family")
-        val version = rs.getString("Version")
-        val id = rs.getString("id")
-        println(s"$name\t$ctfid\t$address\t$zip\t$birthday\t$email\t$mobile\t$tel")
-      }
-    })
+          map.put("Name", rs.getString("Name"))
+          map.put("CardNo", rs.getString("CardNo"))
+          map.put("Descriot", rs.getString("Descriot"))
+          map.put("CtfTp", rs.getString("CtfTp"))
+          map.put("CtfId", rs.getString("CtfId"))
+          map.put("Gender", rs.getString("Gender"))
+          map.put("Birthday", rs.getString("Birthday"))
+          map.put("Address", rs.getString("Address"))
+          map.put("Zip", rs.getString("Zip"))
+          map.put("Dirty", rs.getString("Dirty"))
+          map.put("District1", rs.getString("District1"))
+          map.put("District2", rs.getString("District2"))
+          map.put("District3", rs.getString("District3"))
+          map.put("District4", rs.getString("District4"))
+          map.put("District5", rs.getString("District5"))
+          map.put("District6", rs.getString("District6"))
+          map.put("FirstNm", rs.getString("FirstNm"))
+          map.put("LastNm", rs.getString("LastNm"))
+          map.put("Duty", rs.getString("Duty"))
+          map.put("Mobile", rs.getString("Mobile"))
+          map.put("Tel", rs.getString("Tel"))
+          map.put("Fax", rs.getString("Fax"))
+          map.put("EMail", rs.getString("EMail"))
+          map.put("Nation", rs.getString("Nation"))
+          map.put("Taste", rs.getString("Taste"))
+          map.put("Education", rs.getString("Education"))
+          map.put("Company", rs.getString("Company"))
+          map.put("CTel", rs.getString("CTel"))
+          map.put("CAddress", rs.getString("CAddress"))
+          map.put("CZip", rs.getString("CZip"))
+          map.put("Family", rs.getString("Family"))
+          map.put("Version", rs.getString("Version"))
+          map.put("id", rs.getString("id"))
+
+          val response = client.prepareIndex("customers", "customer").setSource(map).execute().actionGet()
+          println(s"${response.getId} - ${response.getIndex} - ${response.getType} - ${response.getVersion}")
+        }
+      })
+    } finally {
+      node.close()
+    }
+
   }
 }
